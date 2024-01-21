2c928c0cc3a2afa45e0c5071308867c5c5e18b9a
juratali <juratbekxudayberganov9@gmail.com>
2024-01-21 22:47:39 +0500

    first commit

---
+++ http-api-gateway/src/app.module.ts
@@ -0,0 +1,9 @@
+import { Module } from "@nestjs/common";
+
+import { UsersModule } from "./users/users.module";
+import { NatsClientModule } from "./nats-client/nats-client.module";
+
+@Module({
+  imports: [NatsClientModule, UsersModule],
+})
+export class AppModule {}
